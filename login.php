0c167176ad001b9fc8fb91a900b6a6a454283055
irzaldizal <salbert804@gmail.com>
2022-01-18 09:11:11 +0700

    menambahkan

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+<form class="pure-form">
+    <fieldset>
+        <legend>Login</legend>
+        <input type="email" placeholder="Masukkan email anda" />
+        <input type="password" placeholder="Masukkan password anda" />
+        <label for="default-remember">
+            <input type="checkbox" id="default-remember" /> Remember me
+        </label>
+        <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+    </fieldset>
+</form>
+</body>
+</html>
